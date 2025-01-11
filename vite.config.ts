5d17a398a2b7f925f31ff4414b68534dfbecc2a8
Sandrico Provo <Sprovo@outlook.com>
2025-01-11 14:27:12 -0400

    chore: adds testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
+	plugins: [vitePreprocess(), sveltekit(), svelteTesting()],
 	test: {
+		environment: 'jsdom',
+		setupFiles: ['./vitest-setup.ts'],
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
