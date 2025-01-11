37214c1ffd0e7ffb3b8c572514da9b08a33893f2
Sandrico Provo <Sprovo@outlook.com>
2025-01-11 14:20:50 -0400

    feat(styles): adds css reset

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../styles/main.css';
+
+	const { children } = $props();
+</script>
+
+<div>
+	<header></header>
+	{@render children?.()}
+	<footer></footer>
+</div>
+
+<style lang="postcss">
+</style>
